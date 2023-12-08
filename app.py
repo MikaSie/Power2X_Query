0856e4bb4e3574cdd0949832e6fa5e69101ec61f
MikaSie <mikasie6@gmail.com>
2023-12-08 17:23:11 +0100

    update

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from PIL import Image
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-
-def get_pdf_text(pdf_docs: list) -> str:
+def get_pdf_text(pdf_docs):
   """Takes pdf documents and returns raw texts.
 
   Parameters:
@@ -26,15 +26,35 @@ def get_pdf_text(pdf_docs: list) -> str:
   return text
 
 
-def get_text_chunks(raw_text):
-  text_splitter = CharacterTextSplitter(
+def get_text_chunks(raw_text, tokenizer):
+  """ Takes raw text and returns text chunks.
+  
+  Parameters:
+    raw_text(str): String of text from document.
+    tokenizer(AutoTokenizer): Tokenizer from huggingface.
 
-  )
+  
+  Returns:
+    chunks(list): List of chunks (str) of 505 tokens.
+  
+  """
+  text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+    tokenizer, 
+    chunk_size=505, 
+    chunk_overlap=50,
+)
+  
+  chunks = text_splitter.split_text(raw_text)
 
+  return chunks
 
 
 
 def main():
+  load_dotenv()
+  #API TOKEN GEBRUIKEN
+  tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xxl")
+
 
   st.set_page_config(page_title = "Power2X Query Tool",
                      page_icon = 'docs/Power2X-Logo.png')
@@ -48,7 +68,7 @@ def main():
   with st.sidebar:
     st.subheader("Your documents")
 
-    pdf_docs = st.file_uploader("Upload your PDF here and click on 'Process'")
+    pdf_docs = st.file_uploader("Upload your PDF here and click on 'Process'", accept_multiple_files= True)
 
     if st.button("Process"):
       with st.spinner("Processing"):
@@ -56,6 +76,7 @@ def main():
         raw_text = get_pdf_text(pdf_docs)
 
         #GET CHUNKS
+        text_chunks = get_text_chunks(raw_text, tokenizer)
 
         # CREATE VECTOR STORES
 
